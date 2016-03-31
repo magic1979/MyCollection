2e873bba6380038bd0142680a293beb7b5c24c74
magic1979 <salvatore.bruni@gmail.com>
2016-03-31 18:10:37 +0200

    45454
    4545

--- www/js/index.js
+++ www/js/index.js
@@ -24,46 +24,6 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
 	
-	document.body.style.height = screen.availHeight + 'px';
-		
-	$('#valore').focus(function(){
-		myScroll2.scrollToElement("#valore", "1s");
-	});
-		$('#materiale').focus(function(){
-		myScroll2.scrollToElement("#materiale", "1s");
-	});
-		$('#descrizioneF').focus(function(){
-		myScroll2.scrollToElement("#descrizioneF", "1s");
-	});
-		$('#descrizioneR').focus(function(){
-		myScroll2.scrollToElement("#descrizioneR", "1s");
-	});
-		$('#descrizioneB').focus(function(){
-		myScroll2.scrollToElement("#descrizioneB", "1s");
-	});
-		$('#rarita').focus(function(){
-		myScroll2.scrollToElement("#rarita", "1s");
-	});
-		$('#posseduta').focus(function(){
-		myScroll2.scrollToElement("#posseduta", "1s");
-	});
-		$('#dimensione').focus(function(){
-		myScroll2.scrollToElement("#dimensione", "1s");
-	});
-		$('#statoconservazione').focus(function(){
-		myScroll2.scrollToElement("#statoconservazione", "1s");
-	});
-		$('#valore2').focus(function(){
-		myScroll2.scrollToElement("#valore2", "1s");
-	});
-		$('#anno').focus(function(){
-		myScroll2.scrollToElement("#anno", "1s");
-	});
-	
-	$('#tiratura').focus(function(){
-		myScroll2.scrollToElement("#tiratura", "1s");
-	});
-		
 		
 		var provenienza = getParameterByName('id');
 		
@@ -568,6 +528,46 @@ function carica2(){
 			myScroll2.refresh();
 				setTimeout (function(){
 					$("#footer2").fadeIn();
+					
+						document.body.style.height = screen.availHeight + 'px';
+		
+						$('#valore').focus(function(){
+							myScroll2.scrollToElement("#valore", "1s");
+						});
+							$('#materiale').focus(function(){
+							myScroll2.scrollToElement("#materiale", "1s");
+						});
+							$('#descrizioneF').focus(function(){
+							myScroll2.scrollToElement("#descrizioneF", "1s");
+						});
+							$('#descrizioneR').focus(function(){
+							myScroll2.scrollToElement("#descrizioneR", "1s");
+						});
+							$('#descrizioneB').focus(function(){
+							myScroll2.scrollToElement("#descrizioneB", "1s");
+						});
+							$('#rarita').focus(function(){
+							myScroll2.scrollToElement("#rarita", "1s");
+						});
+							$('#posseduta').focus(function(){
+							myScroll2.scrollToElement("#posseduta", "1s");
+						});
+							$('#dimensione').focus(function(){
+							myScroll2.scrollToElement("#dimensione", "1s");
+						});
+							$('#statoconservazione').focus(function(){
+							myScroll2.scrollToElement("#statoconservazione", "1s");
+						});
+							$('#valore2').focus(function(){
+							myScroll2.scrollToElement("#valore2", "1s");
+						});
+							$('#anno').focus(function(){
+							myScroll2.scrollToElement("#anno", "1s");
+						});
+						
+						$('#tiratura').focus(function(){
+							myScroll2.scrollToElement("#tiratura", "1s");
+						});
 			}, 500);
 		}, 700);
 
